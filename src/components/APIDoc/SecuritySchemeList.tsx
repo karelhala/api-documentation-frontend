de06e06ac5a83e7a95cf85bdc979aa4a51fef827
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-01-27 12:05:00 -0700

    RHCLOUD-23881 Setup React application

---
+++ src/components/APIDoc/SecuritySchemeList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {OpenAPIV3} from "openapi-types";
+import {Text, TextContent, TextList, TextListItem, TextVariants} from "@patternfly/react-core";
+import {SecurityScheme} from "./SecurityScheme";
+
+export interface SecuritySchemeListProps {
+    schemes: Array<OpenAPIV3.SecuritySchemeObject>;
+}
+
+export const SecuritySchemeList: React.FunctionComponent<SecuritySchemeListProps> = ({schemes}) =>
+    <TextContent>
+        <Text component={TextVariants.h2}>Authentication</Text>
+        <TextList>
+            {schemes.map((s, index) => <TextListItem key={index}>
+                <SecurityScheme key={index} securityScheme={s} />
+            </TextListItem>)}
+        </TextList>
+    </TextContent>;
