007c4d15704db1fad60450a9efe1ba6997a7b460
Tahmid Efaz <tahmidefaz@gmail.com>
2023-07-25 09:07:38 -0400

    resolve baseurl variables (#206)

--- src/components/APIDoc/ServerList.tsx
+++ src/components/APIDoc/ServerList.tsx
@@ -10,8 +10,26 @@ export const ServerList: React.FunctionComponent<ServerListProps> = ({servers}) 
     <TextContent>
         <Text component={TextVariants.p} className="pf-u-my-sm">Base URLs:</Text>
         <TextList isPlain>
-            {servers.map((s, index) => <TextListItem key={index}>
-                {s.url}
+            {servers.map((server, index) => <TextListItem key={index}>
+                {getServerURL(server)}
             </TextListItem>)}
         </TextList>
     </TextContent>;
+
+
+const getServerURL = (server: OpenAPIV3.ServerObject): string => {
+    let serverURL = server.url
+    if (server.description) {
+        serverURL = `${server.description}: ${server.url}`
+    }
+
+    if (!server.variables) {
+        return serverURL
+    }
+
+    for (const variable in server.variables) {
+        serverURL = serverURL.replace(`{${variable}}`, server.variables[variable].default)
+    }
+
+    return serverURL
+}
