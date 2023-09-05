1c29cd0da3bffbb45b836041c3d0a3760da4211d
Dalia Cervantes <dcervant@redhat.com>
2023-09-05 16:45:31 -0500

    Added tag for deprecation

--- src/components/APIDoc/Operation.tsx
+++ src/components/APIDoc/Operation.tsx
@@ -1,26 +1,32 @@
-import React, { useState, useMemo } from 'react';
-import {OpenAPIV3} from "openapi-types";
-import {deRef} from "../../utils/Openapi";
-import { buildCodeSampleData, BuildCodeSampleDataParams } from '../../utils/Snippets';
+import React, { useState, useMemo } from "react";
+import { OpenAPIV3 } from "openapi-types";
+import { deRef } from "../../utils/Openapi";
 import {
-    Grid,
-    GridItem,
-    Text,
-    TextContent,
-    TextVariants,
-    AccordionItem,
-    AccordionToggle, AccordionContent
+  buildCodeSampleData,
+  BuildCodeSampleDataParams,
+} from "../../utils/Snippets";
+import {
+  Grid,
+  GridItem,
+  Text,
+  TextContent,
+  TextVariants,
+  AccordionItem,
+  AccordionToggle,
+  AccordionContent,
+  Label,
 } from "@patternfly/react-core";
 
-import { ParameterView } from './ParameterView';
-import {CodeSamples} from "./CodeSamples";
-import { RequestBodyView } from './RequestBodyView';
-import { ResponseView } from './ResponseView';
+import { ParameterView } from "./ParameterView";
+import { CodeSamples } from "./CodeSamples";
+import { RequestBodyView } from "./RequestBodyView";
+import { ResponseView } from "./ResponseView";
 
-import {Request as RequestFormat} from 'har-format'
+import { Request as RequestFormat } from "har-format";
 
-import { useSnippets } from '../../hooks/useSnippets';
-import { useLanguage } from '../../utils/LanguageContext';
+import { useSnippets } from "../../hooks/useSnippets";
+import { useLanguage } from "../../utils/LanguageContext";
+import InfoCircleIcon from "@patternfly/react-icons/dist/esm/icons/info-circle-icon";
 
 export interface OperationProps {
   verb: string;
@@ -29,32 +35,59 @@ export interface OperationProps {
   document: OpenAPIV3.Document;
 }
 
-export const Operation: React.FunctionComponent<OperationProps> = props => {
+export const Operation: React.FunctionComponent<OperationProps> = (props) => {
   const id = `operation-${props.verb}-${props.path}`;
   const [isExpanded, setExpanded] = useState(false);
-  const {operation, verb, path} = props;
+  const { operation, verb, path } = props;
 
-  return <AccordionItem>
-    <AccordionToggle
-      id={id}
-      isExpanded={isExpanded}
-      onClick={() => setExpanded(prev => !prev)}
-      className="pf-u-py-sm"
+  return (
+    <AccordionItem>
+      <AccordionToggle
+        id={id}
+        isExpanded={isExpanded}
+        onClick={() => setExpanded((prev) => !prev)}
+        className="pf-u-py-sm"
       >
-        {operation.summary && <span className="pf-u-font-weight-normal pf-u-color-100 pf-u-mr-lg">{operation.summary}<br /></span>}
-         <span className="pf-u-font-size-sm pf-u-font-weight-normal pf-u-color-200">{verb.toUpperCase()} {path}</span>
+        {operation.summary && (
+          <span className="pf-u-font-weight-normal pf-u-color-100 pf-u-mr-lg">
+            {operation.summary}
+            {operation.deprecated && (
+              <Label
+                color="red"
+                icon={<InfoCircleIcon />}
+                className="pf-u-ml-lg"
+              >
+                Deprecated
+              </Label>
+            )}
+            <br />
+          </span>
+        )}
+
+        <span className="pf-u-font-size-sm pf-u-font-weight-normal pf-u-color-200">
+          {verb.toUpperCase()} {path}
+        </span>
       </AccordionToggle>
-      { isExpanded && <AccordionContent>
-        <OperationContent {...props} />
-      </AccordionContent>}
-  </AccordionItem>;
+      {isExpanded && (
+        <AccordionContent>
+          <OperationContent {...props} />
+        </AccordionContent>
+      )}
+    </AccordionItem>
+  );
 };
 
-
-const OperationContent: React.FunctionComponent<OperationProps> = ({verb, path, operation, document}) => {
-  const parameters = (operation.parameters || []).map(p => deRef(p, document));
-  const queryParameters = parameters.filter(p => p.in === "query");
-  const pathParameters = parameters.filter(p => p.in === "path");
+const OperationContent: React.FunctionComponent<OperationProps> = ({
+  verb,
+  path,
+  operation,
+  document,
+}) => {
+  const parameters = (operation.parameters || []).map((p) =>
+    deRef(p, document)
+  );
+  const queryParameters = parameters.filter((p) => p.in === "query");
+  const pathParameters = parameters.filter((p) => p.in === "path");
 
   const codeSampleLanguage = useLanguage();
 
@@ -63,11 +96,14 @@ const OperationContent: React.FunctionComponent<OperationProps> = ({verb, path, 
     path: path,
     params: parameters,
     requestBody: operation.requestBody,
-    responses:  operation.responses,
+    responses: operation.responses,
     document: document,
-  }
+  };
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const reqData: RequestFormat = useMemo(() => buildCodeSampleData(codeSampleBuildParams), [verb, path, codeSampleLanguage]);
+  const reqData: RequestFormat = useMemo(
+    () => buildCodeSampleData(codeSampleBuildParams),
+    [verb, path, codeSampleLanguage]
+  );
 
   const snippets = useSnippets(codeSampleLanguage, reqData);
 
@@ -80,23 +116,36 @@ const OperationContent: React.FunctionComponent<OperationProps> = ({verb, path, 
       </GridItem>
       <GridItem className="pf-m-12-col pf-m-7-col-on-xl">
         <Grid hasGutter>
-          { pathParameters.length > 0 &&
+          {pathParameters.length > 0 && (
             <GridItem className="pf-m-12-col">
-              <ParameterView title="Path Parameters" parameters={pathParameters} document={document}/>
+              <ParameterView
+                title="Path Parameters"
+                parameters={pathParameters}
+                document={document}
+              />
             </GridItem>
-          }
-          { queryParameters.length > 0 &&
+          )}
+          {queryParameters.length > 0 && (
             <GridItem className="pf-m-12-col">
-              <ParameterView title="Query Parameters" parameters={queryParameters} document={document}/>
+              <ParameterView
+                title="Query Parameters"
+                parameters={queryParameters}
+                document={document}
+              />
             </GridItem>
-          }
+          )}
         </Grid>
-        { operation.requestBody && <RequestBodyView requestBody={operation.requestBody} document={document} /> }
+        {operation.requestBody && (
+          <RequestBodyView
+            requestBody={operation.requestBody}
+            document={document}
+          />
+        )}
         <ResponseView responses={operation.responses} document={document} />
       </GridItem>
       <GridItem className="pf-m-12-col pf-m-5-col-on-xl pf-u-mt-md-on-xl pf-u-ml-sm-on-xl">
-        <CodeSamples codesnippet={snippets}/>
+        <CodeSamples codesnippet={snippets} />
       </GridItem>
     </Grid>
   );
-  }
+};
